50fd172fb74b54f52c55cbb1c7d26f6c24b6392c
Luis Burgos <luisarturo.burgos@gmail.com>
2016-03-23 12:05:36 -0600

    Add attributes to domain classes

--- app/src/main/java/com/snotsoft/hungrr/domain/Restaurant.java
+++ app/src/main/java/com/snotsoft/hungrr/domain/Restaurant.java
@@ -1,8 +1,32 @@
 package com.snotsoft.hungrr.domain;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by luisburgos on 21/03/16.
  */
 public class Restaurant {
+
+    @SerializedName("id")
+    private Long id;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("type")
+    private String type;
+
+    @SerializedName("address")
+    private String address;
+
+    @SerializedName("location_lat")
+    private double locationLat;
+
+    @SerializedName("location_lng")
+    private double locationLng;
+
+    @SerializedName("profile_image")
+    private String profileImage;
+
 
 }
--- app/src/main/java/com/snotsoft/hungrr/domain/User.java
+++ app/src/main/java/com/snotsoft/hungrr/domain/User.java
@@ -4,13 +4,71 @@ import com.google.gson.annotations.SerializedName;
 
 public class User {
 
+    @SerializedName("id")
+    private Long id;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("last_name")
+    private String lastName;
+
+    @SerializedName("gender")
+    private String gender;
+
+    @SerializedName("email")
+    private String email;
+
     @SerializedName("username")
     private String username;
 
     @SerializedName("password")
     private String password;
 
+    @SerializedName("token_session")
+    private String tokeSession;
+
     public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getUsername() {
@@ -27,5 +85,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getTokeSession() {
+        return tokeSession;
+    }
+
+    public void setTokeSession(String tokeSession) {
+        this.tokeSession = tokeSession;
     }
 }
