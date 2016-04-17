811e44df1abf974f4e88cf9133e0c3d98acc49e4
elsypinzonv <elsy_pinzonv@hotmail.com>
2016-04-17 18:33:41 -0500

    fragment favoritos

--- app/src/main/java/com/snotsoft/hungrr/view/contracts/FavoritesContract.java
+++
@@ -1,7 +0,0 @@
-package com.snotsoft.hungrr.view.contracts;
-
-/**
- * Created by Elsy on 17/04/2016.
- */
-public interface FavoritesContract {
-}
--- app/src/main/java/com/snotsoft/hungrr/view/fragments/FavoritesFragment.java
+++ app/src/main/java/com/snotsoft/hungrr/view/fragments/FavoritesFragment.java
@@ -1,23 +1,16 @@
 package com.snotsoft.hungrr.view.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.snotsoft.hungrr.R;
 import com.snotsoft.hungrr.domain.Restaurant;
 import com.snotsoft.hungrr.restaurants.RestaurantsLowLevelContract;
-import com.snotsoft.hungrr.restaurants.RestaurantsPresenter;
-import com.snotsoft.hungrr.restaurants.restaurant.RestaurantProfile;
-import com.snotsoft.hungrr.utils.Injection;
-import com.snotsoft.hungrr.view.adapters.RestaurantsAdapter;
-import com.snotsoft.hungrr.view.contracts.FavoritesContract;
+import com.snotsoft.hungrr.view.adapters.FavoritesAdapter;
 import com.snotsoft.hungrr.view.listeners.RestaurantItemListener;
 
 import java.util.ArrayList;
@@ -26,10 +19,10 @@ import java.util.List;
 /**
  * Created by Elsy on 17/04/2016.
  */
-public class FavoritesFragment extends Fragment implements FavoritesContract {
+public class FavoritesFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
-    private RestaurantsAdapter mAdapter;
+    private FavoritesAdapter mAdapter;
     private RestaurantsLowLevelContract.UserActionsListener mActionsListener;
 
     public FavoritesFragment() {
@@ -42,14 +35,14 @@ public class FavoritesFragment extends Fragment implements FavoritesContract {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAdapter = new RestaurantsAdapter(getActivity(), new ArrayList<Restaurant>(0), new RestaurantItemListener() {
+        mAdapter = new FavoritesAdapter(getActivity(), new ArrayList<Restaurant>(0), new RestaurantItemListener() {
             @Override
             public void onRestaurantClick(Restaurant clickedRestaurant) {
                 mActionsListener.openRestaurantProfile(clickedRestaurant);
             }
         });
     }
-/*
+
     @Override
     public void onResume() {
         super.onResume();
@@ -60,47 +53,24 @@ public class FavoritesFragment extends Fragment implements FavoritesContract {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         setRetainInstance(true);
-        mActionsListener = new RestaurantsPresenter(
+    /*    mActionsListener = new RestaurantsPresenter(
                 this,
                 Injection.provideRestaurantsInteractor(),
                 Injection.provideUserSessionManager(getActivity().getApplicationContext()),
                 Injection.provideLocationPreferencesManager(getActivity().getApplicationContext())
-        );
+        ); */
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_restaurants_low_level, container, false);
+        View root = inflater.inflate(R.layout.fragment_favorites, container, false);
 
-        mRecyclerView = (RecyclerView) root.findViewById(R.id.restaurants);
+        mRecyclerView = (RecyclerView) root.findViewById(R.id.favorites);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
         return root;
     }
 
-
-    @Override
-    public void setProgressIndicator(boolean active) {
-
-    }
-
-    @Override
-    public void showRestaurants(List<Restaurant> restaurants) {
-        mAdapter.replaceData(restaurants);
-    }
-
-    @Override
-    public void showRestaurantProfileUI(String id) {
-        Intent intent = new Intent().setClass(getActivity().getApplicationContext(), RestaurantProfile.class);
-        intent.putExtra("restaurantID", id);
-        startActivity(intent);
-    }
-
-    @Override
-    public void showErrorMessage(String message) {
-        Snackbar.make(getView(), message, Snackbar.LENGTH_SHORT).show();
-    }*/
 }
