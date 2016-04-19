a7f27158a64c3ceeca323e6790e21d92fd1d9f4f
Luis Burgos <luisarturo.burgos@gmail.com>
2016-04-18 21:08:56 -0500

    Add back pressed on RestaurantProfile

--- app/src/main/java/com/snotsoft/hungrr/restaurants/restaurant/RestaurantProfile.java
+++ app/src/main/java/com/snotsoft/hungrr/restaurants/restaurant/RestaurantProfile.java
@@ -3,18 +3,35 @@ package com.snotsoft.hungrr.restaurants.restaurant;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.snotsoft.hungrr.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class RestaurantProfile extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+
+    private String restaurantID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        restaurantID = getIntent().getExtras().getString("restaurantID");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
 }
