665e135436ade66411fe80dd95f86f008d4d1a8f
elsypinzonv <elsy_pinzonv@hotmail.com>
2016-05-03 17:55:04 -0500

    se arreglo repetición de menus en onResume

--- app/src/main/java/com/snotsoft/hungrr/restaurants/restaurant/RestaurantProfile.java
+++ app/src/main/java/com/snotsoft/hungrr/restaurants/restaurant/RestaurantProfile.java
@@ -22,7 +22,6 @@ import com.snotsoft.hungrr.domain.Menu;
 import com.snotsoft.hungrr.domain.Restaurant;
 import com.snotsoft.hungrr.domain.RestaurantPhone;
 import com.snotsoft.hungrr.domain.Schedule;
-import com.snotsoft.hungrr.domain.Section;
 import com.snotsoft.hungrr.utils.Injection;
 import com.snotsoft.hungrr.utils.ResourceCompatMethod;
 import com.squareup.picasso.Picasso;
@@ -50,6 +49,7 @@ public class RestaurantProfile extends AppCompatActivity implements RestaurantPr
     private Restaurant mRestaurant;
     private RestaurantProfileContract.UserActionsListener mActionsListener;
     private boolean isAlreadyFetchDetails;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,13 +82,14 @@ public class RestaurantProfile extends AppCompatActivity implements RestaurantPr
             }
         });
         setData(mRestaurant);
+        mActionsListener.loadRestaurantInformation(mRestaurantID);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         isAlreadyFetchDetails = false;
-        mActionsListener.loadRestaurantInformation(mRestaurantID);
+
     }
 
     @Override
@@ -169,12 +170,13 @@ public class RestaurantProfile extends AppCompatActivity implements RestaurantPr
     }
 
     private void setMenus(ArrayList<Menu> menus) {
-        if(menus != null && !menus.isEmpty()){
-            menusHeaderTextView.setVisibility(View.VISIBLE);
-            for(Menu menu : menus){
-                setMenu(menu);
+            if(menus != null && !menus.isEmpty()){
+                menusHeaderTextView.setVisibility(View.VISIBLE);
+                for(Menu menu : menus){
+                    setMenu(menu);
+                }
             }
-        }
+
     }
 
     private void setMenu(Menu menu) {
