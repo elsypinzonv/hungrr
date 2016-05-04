6aa20f69e862eb0c208c385fb5c38aa323506472
elsypinzonv <elsy_pinzonv@hotmail.com>
2016-05-04 10:15:13 -0500

    corrigio error de favorito en el perfil

--- app/src/main/java/com/snotsoft/hungrr/explore/restaurant/RestaurantProfile.java
+++ app/src/main/java/com/snotsoft/hungrr/explore/restaurant/RestaurantProfile.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.snotsoft.hungrr.R;
@@ -139,6 +140,7 @@ public class RestaurantProfile extends AppCompatActivity implements RestaurantPr
             mFab.setRippleColor(resourceCompat.getColorCompat(R.color.colorAccent));
             mFab.setImageResource(R.mipmap.ic_favorite_border);
         }
+        mRestaurant.setIsFavorite(isFavorite);
     }
 
     private void setImage(String imageURL){
